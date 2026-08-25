e1b19beae07cea0d82c44affc617ddea715c3927
Swinek <maciejsa10@gmail.com>
2026-08-25 22:33:22 +0200

    fix(tests): add force click to checkbox and submit button in firefox e2e

--- Client/tests/pages/addNewSamplePage.ts
+++ Client/tests/pages/addNewSamplePage.ts
@@ -20,8 +20,9 @@ export class NewSamplePage extends BaseAddNewPageDragable {
     await this.page
       .getByRole("option", { name: "Upturn" })
       .click({ force: true });
-    await this.page.keyboard.press("Escape");
-    await this.page.getByRole("checkbox", { name: "Save as recipe:" }).click();
+    await this.page
+      .getByRole("checkbox", { name: "Save as recipe:" })
+      .click({ force: true });
     await this.page
       .getByRole("textbox", { name: "Recipe Name:" })
       .fill("first-recipe");
@@ -33,7 +34,9 @@ export class NewSamplePage extends BaseAddNewPageDragable {
     await this.page
       .getByRole("textbox", { name: "Comment" })
       .fill("This is comment");
-    await this.page.getByRole("button", { name: "Add process" }).click();
+    await this.page
+      .getByRole("button", { name: "Add process" })
+      .click({ force: true });
     await expect(this.page.getByText(this.successMessage)).toBeVisible();
   }
 }
